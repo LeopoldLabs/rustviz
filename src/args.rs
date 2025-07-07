65bca99928611d60d4286d621ea024e4982aa793
pold-io <projekte@leoj.de>
2025-07-08 00:40:11 +0200

    Update args.rs

--- src/args.rs
+++ src/args.rs
@@ -1,15 +1,17 @@
 //! cli args + parser
 
-use std::path::{PathBuf};
+use clap::{Parser, ValueEnum};
+use std::path::PathBuf;
 
-use clap::Parser;
+#[derive(Clone, ValueEnum)]
+pub enum Level {
+    Crate,
+    Module,
+}
 
 #[derive(Parser)]
 pub(crate) struct Args {
-    pub filepath: PathBuf,
+    pub project_path: PathBuf,
 
-    pub max_depth: Option<u32>,
-
-    #[arg(long, action)]
-    pub include_private: bool,
+    pub level: Level,
 }
